3850fd29228f24be2f43bcba0e3f2f5bee75499a
UrielCuriel <urielcuriel@outlook.com>
2023-11-27 15:25:54 -0600

    Add .tmuxinator.yml and update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  preset: "ts-jest",
+  testEnvironment: "node",
+  testMatch: ["**/*.spec.ts"],
+  testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/dist/"],
 };
